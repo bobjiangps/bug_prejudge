352bf9ba8cb9f1155c2c488c72e8d024e2e5379a
bobjiangps <jbsv43@sina.com>
2019-07-09 18:36:32 +0800

    check the test round to mark normal or unnormal according to offset and quantile

--- test_round_pass_rate.py
+++ test_round_pass_rate.py
@@ -1,12 +1,12 @@
 from utils.mysql_helper import MysqlConnection
 from configuration.config import Config
 import os
+import pandas as pd
 
 
-def generate_history_regression_data(dbconn, project_id, filename="history_regression.csv"):
+def generate_history_regression_data(dbconn, project_id, filepath):
     generate_flag = Config.load_env()["generate_history_regression"]
-    history_regression_file = os.path.join(os.getcwd(), "data", filename)
-    if not os.path.exists(history_regression_file):
+    if not os.path.exists(filepath):
         generate_flag = True
     data_folder = os.path.join(os.getcwd(), "data")
     if not os.path.exists(data_folder):
@@ -17,14 +17,14 @@ def generate_history_regression_data(dbconn, project_id, filename="history_regre
         # query history data of 6 month for reference
         six_month_regression_sql = "select * from test_rounds where project_id=%d and DATE_SUB(CURDATE(), INTERVAL 6 MONTH) <= date(start_time) and end_time is not NULL;" % int(project_id)
         history_regression = dbconn.get_all_results_from_database(six_month_regression_sql)
-        with open(history_regression_file, "w") as f:
+        with open(filepath, "w") as f:
             f.write(",".join(history_regression[0].keys()) + "\n")
             for row in history_regression:
                 new_row = [str(x).replace("\r", " ").replace("\n", " ").replace(",", " ") for x in row.values()]
                 f.write(",".join(new_row) + "\n")
-        print("there are %d rows in database when query the sql" % len(history_regression))
+        print("----there are %d rows in database when query the sql----\n" % len(history_regression))
     else:
-        print("----NOT generate history regression data----")
+        print("----NOT generate history regression data----\n")
 
 
 if __name__ == "__main__":
@@ -33,4 +33,23 @@ if __name__ == "__main__":
     db = MysqlConnection().connect("local_regression")
     current_test_round = db.get_first_result_from_database("select * from test_rounds where id=%d;" % int(test_round_id))
     print("specified test round infomation:\n", current_test_round)
-    generate_history_regression_data(db, current_test_round["project_id"])
+    history_regression_file = os.path.join(os.getcwd(), "data", "history_regression.csv")
+    generate_history_regression_data(db, current_test_round["project_id"], history_regression_file)
+
+    history = pd.read_csv(history_regression_file)
+    to_drop = ["counter", "sprint", "exist_regression_report"]
+    history.drop(columns=to_drop, inplace=True)
+    # loc retrieves only the rows that matches the condition
+    # print(history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].count)
+    # 分位数，这里代表有90%的值都大于这个数字，那么如果新的值落在这个值以下，可以考虑为可能异常；比如总数14个，N1到N14，(14-1)/10+1=2.3, N2+(N3-N2)*0.3就是10%分位数
+    print(history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].pass_rate.quantile(.1))
+    # where returns the whole dataframe, replacing rows that don't match the condition as NaN by default
+    # print(history.where(history["test_suite_id"] == current_test_round["test_suite_id"]).count())
+    # print(history.where(history["test_suite_id"] == current_test_round["test_suite_id"]).pass_rate.mean())
+
+    pass_rate_quantile_ten_percent = history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].pass_rate.quantile(.1)
+    average_pass_rate = history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].pass_rate.mean()
+    if current_test_round["pass_rate"] <= pass_rate_quantile_ten_percent or (average_pass_rate - current_test_round["pass_rate"]) > Config.load_env()["pass_rate_offset"] * 100:
+        print("Unnormal Test Round !!! need to check error messages first")
+    else:
+        print("Normal Test Round..")
