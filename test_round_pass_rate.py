b1302610ff016e9806ed4d2c0d421f02f5849949
bobjiangps <jbsv43@sina.com>
2019-07-10 18:11:33 +0800

    show network error percentage or most element failures when unnormal round

--- test_round_pass_rate.py
+++ test_round_pass_rate.py
@@ -1,16 +1,15 @@
 from utils.mysql_helper import MysqlConnection
 from configuration.config import Config
+from collections import Counter
 import os
 import pandas as pd
+import re
 
 
 def generate_history_regression_data(dbconn, project_id, filepath):
     generate_flag = Config.load_env()["generate_history_regression"]
     if not os.path.exists(filepath):
         generate_flag = True
-    data_folder = os.path.join(os.getcwd(), "data")
-    if not os.path.exists(data_folder):
-        os.mkdir(data_folder)
 
     if generate_flag:
         print("----generate history regression data----")
@@ -26,15 +25,36 @@ def generate_history_regression_data(dbconn, project_id, filepath):
     else:
         print("----NOT generate history regression data----\n")
 
+def generate_test_round_errors_data(dbconn, round_id, filepath):
+    test_round_errors_sql = "SELECT * FROM automation_case_results where automation_script_result_id in (select id from automation_script_results where test_round_id=%d) and result = 'failed';" % int(round_id)
+    # test_round_errors_sql = "select * from automation_case_results where result='failed' and error_message like '%Timeout%';"  # to be removed
+    print("----generate test round errors data----")
+    test_round_erros = dbconn.get_all_results_from_database(test_round_errors_sql)
+    if len(test_round_erros) == 0:
+        print("no errors in this test round with id: %d" % int(round_id))
+        return False
+    else:
+        with open(filepath, "w") as f:
+            f.write(",".join(test_round_erros[0].keys()) + "\n")
+            for row in test_round_erros:
+                new_row = [str(x).replace("\r", " ").replace("\n", " ").replace(",", " ") for x in row.values()]
+                f.write(",".join(new_row) + "\n")
+        print("----there are %d rows in database when query the sql----\n" % len(test_round_erros))
+        return True
+
 
 if __name__ == "__main__":
     # test_round_id = input("Please input test round id you want to check:\n")  # 97585 for test
     test_round_id = 97585
-    db = MysqlConnection().connect("local_regression")
-    current_test_round = db.get_first_result_from_database("select * from test_rounds where id=%d;" % int(test_round_id))
+    response = {"id": test_round_id, "message": None, "cases": None}
+    data_folder = os.path.join(os.getcwd(), "data")
+    if not os.path.exists(data_folder):
+        os.mkdir(data_folder)
+    regression_db = MysqlConnection().connect("local_regression")
+    current_test_round = regression_db.get_first_result_from_database("select * from test_rounds where id=%d;" % int(test_round_id))
     print("specified test round infomation:\n", current_test_round)
     history_regression_file = os.path.join(os.getcwd(), "data", "history_regression.csv")
-    generate_history_regression_data(db, current_test_round["project_id"], history_regression_file)
+    generate_history_regression_data(regression_db, current_test_round["project_id"], history_regression_file)
 
     history = pd.read_csv(history_regression_file)
     to_drop = ["counter", "sprint", "exist_regression_report"]
@@ -48,8 +68,8 @@ if __name__ == "__main__":
         # loc retrieves only the rows that matches the condition
         # print(history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].count)
         # 分位数，这里代表有90%的值都大于这个数字，那么如果新的值落在这个值以下，可以考虑为可能异常；比如总数14个，N1到N14，(14-1)/10+1=2.3, N2+(N3-N2)*0.3就是10%分位数
-        print("10% quantile is:", history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].pass_rate.quantile(.1))
-        print("current pass rate is:", current_test_round["pass_rate"])
+        print("10% quantile is:", "%.2f%%" % history.loc[history["test_suite_id"] == current_test_round["test_suite_id"]].pass_rate.quantile(.1))
+        print("current pass rate is:", "%.2f%%" % current_test_round["pass_rate"])
         # where returns the whole dataframe, replacing rows that don't match the condition as NaN by default
         # print(history.where(history["test_suite_id"] == current_test_round["test_suite_id"]).count())
         # print(history.where(history["test_suite_id"] == current_test_round["test_suite_id"]).pass_rate.mean())
@@ -62,3 +82,44 @@ if __name__ == "__main__":
         else:
             print("Normal Test Round..")
             normal_round = True
+
+    normal_round = False  # debug, will be removed
+    if normal_round:
+        #todo
+        print("todo...")
+    else:
+        test_round_errors_file = os.path.join(os.getcwd(), "data", "test_round_errors.csv")
+        # generate_error_result = generate_test_round_errors_data(regression_db, test_round_id, test_round_errors_file)
+        generate_error_result = True  # debug, will be removed
+        if generate_error_result:
+            network_error_keyword = ["Net::ReadTimeout", "Request Timeout"]
+            round_errors = pd.read_csv(test_round_errors_file)
+            # print(round_errors[round_errors["error_message"].str.contains("|".join(network_error_keyword), flags=re.IGNORECASE, regex=True)])
+            round_network_errors = round_errors[round_errors["error_message"].str.contains("|".join(network_error_keyword), flags=re.IGNORECASE, regex=True)]
+            network_error_percentage = round_network_errors.id.count()/round_errors.id.count()
+            print("network error percentage is:", "%.2f%%" % (network_error_percentage * 100))
+            if network_error_percentage > 0.5:
+                response["message"] = "More than 50%% of failures are caused by network issue, please check environment then rerun test round %d" % test_round_id
+                print("Attention!!! %s" % response["message"])
+            else:
+                # element_error_keyword = [".*Execute - wait \w*::\w* to present.*", ".*The element.*does not exist.*", ".*Execute - open .*::.*- failed.*", ".*Execute - select .*::.*- failed.*", ".*Execute - get .*::.*- failed.*"]
+                # round_element_errors = round_errors[round_errors["error_message"].str.match("|".join(element_error_keyword), flags=re.IGNORECASE)]
+                # print(round_element_errors)
+                # element_error_keyword = [".*Execute - get (.*::.*)- failed.*"]
+                # round_element_errors = round_errors[round_errors["error_message"].str.extract(element_error_keyword[0], flags=re.IGNORECASE, expand=False)]
+                # print(round_element_errors)
+                element_error_keyword = [".*Execute - wait (\w*::\w*) to present.*", ".*Execute - open .* (\w*::\w*) .*- failed.*", ".*Execute - select .* (\w*::\w*) .*- failed.*", ".*Execute - get .* (\w*::\w*) .*- failed.*"]
+                round_element_errors_match = round_errors[round_errors["error_message"].str.match("|".join(element_error_keyword), flags=re.IGNORECASE)]
+                round_element_errors_extract = round_element_errors_match.error_message.str.extract("|".join(element_error_keyword), flags=re.IGNORECASE, expand=False)
+                print(round_element_errors_extract)  # to be removed
+                round_element_errors_record = []
+                for seq in round_element_errors_extract:
+                    print(seq)  # to be removed
+                    for item in round_element_errors_extract[seq]:
+                        if str(item) != "nan":
+                            print(item)  # to be removed
+                            round_element_errors_record.append(item)
+                response["message"] = "The most failures related to element: %s" % Counter(round_element_errors_record).most_common(1)[0][0]
+                print("Attention!!! %s" % response["message"])
+
+
