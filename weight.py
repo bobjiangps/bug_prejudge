eff088af7881d32dadeb1bf96a5378e78ff45c7d
bobjiangps <jbsv43@sina.com>
2019-09-23 14:45:44 +0800

    limit projects when auto-generate weights

--- weight.py
+++ weight.py
@@ -12,10 +12,12 @@ if __name__ == "__main__":
     else:
         triage_history_file = os.path.join(os.getcwd(), "data", "triage_history.csv")
         triage_history = pd.read_csv(triage_history_file)
+        include_projects = [p.strip() for p in Config.load_env("apply_to_project").split(",")] if Config.load_env("apply_to_project") else []
         for p in triage_history["project"].unique():
-            project_history = triage_history[triage_history["project"] == p]
-            if len(project_history) >= Config.load_env("triage_trigger_ml"):
-                projects.append(p)
+            if p in include_projects:
+                project_history = triage_history[triage_history["project"] == p]
+                if len(project_history) >= Config.load_env("triage_trigger_ml"):
+                    projects.append(p)
 
     for project in projects:
         project_triage_history_file = os.path.join(os.getcwd(), "data", "triage_history_%s.csv" % project)
@@ -23,7 +25,7 @@ if __name__ == "__main__":
         project_triage_history["offset"] = 1
         project_triage_history["is_bug"] = project_triage_history["triage_type"].apply(lambda x: 1 if x.lower() == "product error" else 0)
         project_triage_history.drop(columns=["triage_type"], inplace=True)
-        weight = asc_with_target(project_triage_history.iloc[:, :-1], project_triage_history["is_bug"], step_size=0.001, target=0.7, timeout=360)
+        weight = asc_with_target(project_triage_history.iloc[:, :-1], project_triage_history["is_bug"], step_size=0.01, target=0.9, timeout=900, lean_to_bug=True)
         weight = weight.flatten().tolist()
         columns = list(project_triage_history.iloc[:, :-1].columns.values)
         project_parameter_file = os.path.join(os.getcwd(), "data", "parameter_%s.csv" % project)
