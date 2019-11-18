a5fab580857a0fc90e4a0c2e30ec54aa0072a204
bobjiangps <jbsv43@sina.com>
2019-11-18 11:48:11 +0800

    update sh file to start service

--- configuration/start_service_after_reboot.sh
+++ configuration/start_service_after_reboot.sh
@@ -4,8 +4,8 @@
 ## m h  dom mon dow   command
 #00 01 * * * find /home/test/screen_shots/. -mtime +180 -exec rm -rf {} \;
 #30 01 * * * find /home/test/slave_logs/. -mtime +180 -exec rm -rf {} \;
-#00 02 1 * * /home/active/prejudge_service/venv/bin/python /home/active/prejudge_service/available.py > /home/active/prejudge_service/start.log
-#@reboot /home/active/reboot_services/file_service.sh
+#00 02 1 * * /home/bob/prejudge_service/venv/bin/python /home/bob/prejudge_service/available.py > /home/bob/prejudge_service/start.log
+#@reboot /home/bob/reboot_services/start_service_after_reboot.sh
 
 NGINX_ROOT="/usr"
 if ps ax | grep -v grep | grep nginx > /dev/null
@@ -19,6 +19,6 @@ if ps ax | grep -v grep | grep uwsgi > /dev/null
 then
   echo "uwsgi running"
 else
-  source /home/active/prejudge_service/venv/bin/activate
-  uwsgi /home/active/prejudge_service/configuration/uwsgi.ini
+  source /home/bob/prejudge_service/venv/bin/activate
+  uwsgi /home/bob/prejudge_service/configuration/uwsgi.ini
 fi
