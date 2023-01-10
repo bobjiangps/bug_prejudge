26fafd4a4c5358316559f822dfd02722a022b2fd
bobjiangps <jbsv43@sina.com>
2023-01-10 18:13:50 +0800

    add new api to detect image but only return text info now

--- detect/urls.py
+++ detect/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path(r'parameters/', views.PublicParameters.as_view(), name='public_parameters'),
     path(r'available_models/', views.AvailableModels.as_view(), name='available_models'),
+    re_path(r'detect/?', views.Detection.as_view(), name='detection'),
 ]
--- detect/views.py
+++ detect/views.py
@@ -2,6 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from API.settings import MODELS
+from pathlib import Path
+from sights.lib.visual.pred import predict
+import base64
+import time
 
 
 class PublicParameters(APIView):
@@ -24,4 +28,26 @@ class AvailableModels(APIView):
         data = {}
         for project in MODELS:
             data[project] = MODELS[project].keys()
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class Detection(APIView):
+
+    def post(self, request):
+        start = time.time()
+        src_category, model_category = request.data.keys()
+        img_path = Path.cwd().joinpath("static", "cache_images", f"{int(time.time())}.png")
+        with open(img_path, "wb") as f:
+            f.write(base64.b64decode(request.data[src_category].split(",")[1]))
+        model_map = {
+            "Elements": "element",
+            "Icons": "icon"
+        }
+        results, labels, _ = predict(list(MODELS[model_map[request.data[model_category]]].values())[0], str(img_path))
+        end = time.time()
+        data = {
+            "summary": labels,
+            "detail": results,
+            "duration": end-start
+        }
         return Response(data, status=status.HTTP_200_OK)
