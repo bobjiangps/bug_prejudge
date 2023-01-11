8dd7d8197976a52bb73700acc40192b9eaee94f6
bobjiangps <jbsv43@sina.com>
2023-01-11 18:16:07 +0800

    draw boxes on image which to detect

--- detect/views.py
+++ detect/views.py
@@ -52,5 +52,26 @@ class Detection(APIView):
             "duration": end-start
         }
         print(data)
-        shutil.copy(origin_path, detect_path)
+        # shutil.copy(origin_path, detect_path)
+        # img_init = cv2.imread(str(origin_path))
+        # cv2.imwrite(detect_path, img_init)
+        cv2.imwrite(detect_path, self.draw(cv2.imread(str(origin_path)), data))
         return Response({"url": "static" + detect_path.split("static")[-1]}, status=status.HTTP_200_OK)
+
+    @staticmethod
+    def draw(im, contents):
+        hexs = ("#FF3838", "#FF9D97", "#FF701F", "#FFB21D", "#CFD231", "#48F90A", "#92CC17", "#3DDB86", "#1A9334", "#00D4BB")
+        colors = {}
+        for seq, s in enumerate(list(contents["summary"].keys())):
+            c_temp = tuple(int(hexs[seq][1 + i:1 + i + 2], 16) for i in (0, 2, 4))
+            colors[s] = (c_temp[2], c_temp[1], c_temp[0])
+        for item in contents["detail"]:
+            tl = 3 or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1
+            c1, c2 = (int(item["COOR"][0]), int(item["COOR"][1])), (int(item["COOR"][2]), int(item["COOR"][3]))
+            cv2.rectangle(im, c1, c2, colors[item["N"]], thickness=tl, lineType=cv2.LINE_AA)
+            tf = max(tl - 1, 1)
+            t_size = cv2.getTextSize(f"{item['N']}: {item['PR']}", 0, fontScale=tl / 3, thickness=tf)[0]
+            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+            cv2.rectangle(im, c1, c2, colors[item["N"]], -1, cv2.LINE_AA)
+            cv2.putText(im, f"{item['N']}: {item['PR']}", (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        return im
